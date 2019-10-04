c524388d3d6c233ad69659c77bbb3fb4f041ab17
Kohei Kimura <kohei.kimura@nulab.com>
2019-10-04 02:36:15 +0000

    Update TestTokenize

--- 9gc_test.go
+++ 9gc_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -14,7 +15,7 @@ func TestTokenize(t *testing.T) {
 	}{
 		{
 			" 1 ",
-			&Token{TK_NUM, tokenEof, 1, "1"},
+			&Token{TK_NUM, tokenEof, 1, "1", 1},
 		},
 		{
 			"0 + 45 - 5 ",
@@ -22,23 +23,91 @@ func TestTokenize(t *testing.T) {
 				kind: TK_NUM,
 				val:  0,
 				str:  "0",
+				len:  1,
 				next: &Token{
 					kind: TK_RESERVED,
 					str:  "+",
+					len:  1,
 					next: &Token{
 						kind: TK_NUM,
 						val:  45,
 						str:  "45",
+						len:  2,
 						next: &Token{
 							kind: TK_RESERVED,
 							str:  "-",
+							len:  1,
 							next: &Token{
 								kind: TK_NUM,
 								val:  5,
 								str:  "5",
+								len:  1,
 								next: tokenEof,
 							},
 						},
+					},
+				},
+			},
+		},
+		{
+			"5*(9-6)",
+			&Token{
+				kind: TK_NUM,
+				val:  5,
+				str:  "5",
+				len:  1,
+				next: &Token{
+					kind: TK_RESERVED,
+					str:  "*",
+					len:  1,
+					next: &Token{
+						kind: TK_RESERVED,
+						str:  "(",
+						len:  1,
+						next: &Token{
+							kind: TK_NUM,
+							str:  "9",
+							val:  9,
+							len:  1,
+							next: &Token{
+								kind: TK_RESERVED,
+								str:  "-",
+								len:  1,
+								next: &Token{
+									kind: TK_NUM,
+									val:  6,
+									str:  "6",
+									len:  1,
+									next: &Token{
+										kind: TK_RESERVED,
+										str:  ")",
+										len:  1,
+										next: tokenEof,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"42!=42",
+			&Token{
+				kind: TK_NUM,
+				val:  42,
+				str:  "42",
+				len:  2,
+				next: &Token{
+					kind: TK_RESERVED,
+					str:  "!=",
+					len:  2,
+					next: &Token{
+						kind: TK_NUM,
+						val:  42,
+						str:  "42",
+						len:  2,
+						next: tokenEof,
 					},
 				},
 			},
@@ -53,8 +122,15 @@ func TestTokenize(t *testing.T) {
 			}
 
 			if !reflect.DeepEqual(actual, tC.expected) {
-				t.Fatalf("Tokenizing '%s' failed.\nactual:\t%+v\nexpected:\t%+v\n", tC.str, actual, tC.expected)
+				t.Fatalf("Tokenizing '%s' failed.\nactual:\n%+v\nexpected:\n%+v\n", tC.str, showTokens(actual), showTokens(tC.expected))
 			}
 		})
 	}
+}
+
+func showTokens(t *Token) string {
+	if t.next == nil {
+		return ""
+	}
+	return fmt.Sprintf("%+v\n%+v", t, showTokens(t.next))
 }
